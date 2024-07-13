e05f4096f89f1b38b56308df710829c9662f11a1
Piyush Dhanawat <117037208+Piyush26102@users.noreply.github.com>
2024-07-13 14:23:15 +0530

    Add files via upload

---
+++ JS Files/hamburgar.js
@@ -0,0 +1,13 @@
+const hamburgar = document.getElementById ("hamburger");
+        const navList = document.getElementById ("navbar");
+        hamburgar.addEventListener ('click', ()=>{
+           navList.classList.toggle("hidden")
+           const ham_child = hamburgar.firstElementChild;
+           if(navList.classList.contains('hidden')){
+                 ham_child.classList.replace('fa-xmark','fa-bars');
+           }
+           else {
+            ham_child.classList.replace('fa-bars','fa-xmark');
+           }
+        });
+        
