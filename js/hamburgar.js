8427f7edeb72433cb1dce46b8d3a29d8bab8e106
Kushagra <sparrow.kushagra@gmail.com>
2024-07-17 13:46:26 +0530

    UI Refactor [+] fixed hamburger button to work [+} other minor fixex

--- js/hamburgar.js
+++ js/hamburgar.js
@@ -1,13 +1,11 @@
-const hamburgar = document.getElementById ("hamburger");
-        const navList = document.getElementById ("navbar");
-        hamburgar.addEventListener ('click', ()=>{
-           navList.classList.toggle("hidden")
-           const ham_child = hamburgar.firstElementChild;
-           if(navList.classList.contains('hidden')){
-                 ham_child.classList.replace('fa-xmark','fa-bars');
-           }
-           else {
-            ham_child.classList.replace('fa-bars','fa-xmark');
-           }
-        });
-        
+const hamburgar = document.getElementById("ham");
+const navList = document.getElementById("navbar");
+hamburgar.addEventListener("click", () => {
+  navList.classList.toggle("hidden");
+  const ham_child = hamburgar.firstElementChild;
+  if (navList.classList.contains("hidden")) {
+    ham_child.classList.replace("fa-xmark", "fa-bars");
+  } else {
+    ham_child.classList.replace("fa-bars", "fa-xmark");
+  }
+});
